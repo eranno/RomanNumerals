fec340e5a3b0cd646af2a854e9d25c9aa98e7ab3
eranno <erannn@gmail.com>
2012-12-12 09:05:31 +0200

    switch

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -3,9 +3,18 @@
 public class RomanNumerals {
 
 	public String toRoman(int nmber) {
-		if (nmber == 1)
-			return "I";
-		return "II";
+		switch (nmber) {
+			case 1: return "I";
+			case 2: return "II";
+			case 3: return "III";
+			case 4: return "IV";
+			case 5: return "V";
+			case 6: return "VI";
+			case 7: return "VII";
+			case 8: return "VIII";
+			case 9: return "IX";
+			case 10: return "X";
+		}
 	}
 
 }
